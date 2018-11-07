2d56d5794b5951708884b90adaf9796bb7c01569
Tim S <tim.sanders@student.fontys.nl>
2018-11-07 12:41:59 +0100

    First model commit

---
+++ src/main/java/api/Application.java
@@ -0,0 +1,14 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication(scanBasePackages = {"api"})
+@EnableJpaRepositories(basePackages = "api.repositories")
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
